de4c8a47d3f7f743e462d2ca670f6c9bb5dcf187
Nick <93144005+NiRGEsFY@users.noreply.github.com>
2024-01-22 11:49:10 +0300

    Update Labs.cpp

--- BSU.Lab/Labs.cpp
+++ BSU.Lab/Labs.cpp
@@ -15,43 +15,46 @@ public:
 	}
 };
 
-class Book {
-public:
-	string _name;
-	string _author;
-	BookPublisher _bookPublisher;
-	int _year;
-	int _list;
-	Book() {
-		_name = "None";
-		_author = "None";
-		_bookPublisher = BookPublisher();
-		_year = 0;
-		_list = 0;
+Book::Book() {
+	_name = "None";
+	_author = "None";
+	_bookPublisher = "None";
+	_year = 0;
+	_list = 0;
+}
+Book::Book(string name, string author, string bookPublisher, int year, int list) {
+	_name = name;
+	_author = author;
+	_bookPublisher = bookPublisher;
+	_year = year;
+	_list = list;
+}
+void Book::set(Book book) {
+	_name = book._name;
+	_author = book._author;
+	_bookPublisher = book._bookPublisher;
+	_year = book._year;
+	_list = book._list;
+}
+int Book::getList(){
+	return _list;
+}
+int Book::getYear() {
+	return _year;
+}
+string Book::getBookPublisher() {
+	return _bookPublisher;
+}
+string Book::getAuthor() {
+	return _author;
+}
+string Book::getName() {
+	return _name;
+}
+void Book::show(bool trust) {
+	if (trust) {
+		cout << "Название:" << _name << "| автор:" << _author << "| издатель: " << _bookPublisher << "| год: " << _year << "| страницы: " << _list << endl;
+		return;
 	}
-	Book(string name, string author, BookPublisher bookPublisher, int year, int list) {
-		_name = name;
-		_author = author;
-		_bookPublisher = bookPublisher;
-		_year = _year;
-		_list = list;
-	}
-	void set(Book book) {
-		_name = book._name;
-		_author = book._author;
-		_bookPublisher = book._bookPublisher;
-		_year = book._year;
-		_list = book._list;
-	}
-	Book & get() {
-		return *this;
-	}
-	void show(bool trust) {
-		if (trust) {
-			cout << "Book name:" << _name << "| author:" << _author << "| publisher: " << _bookPublisher._name << "| year: " << _year << "| lists: " << _list << endl;
-			return;
-		}
-		cout << "Book name:" << _name << "| author:" << endl;
-	}
-
-};
+	cout << "Название:" << _name << "| автор:" << _bookPublisher << endl;
+}
